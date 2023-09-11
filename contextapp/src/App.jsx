bffa05d991e1578c62d9f936f1e3c9eaec8ee529
Ayomide Akintan <emzyakints2005@gmail.com>
2023-09-11 22:21:17 +0100

    Update

--- contextapp/src/App.jsx
+++ contextapp/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Booklist from './components/Booklist'
+import ThemeContextProvider from './contexts/ThemeContext'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar />
-    <Booklist />
+      <ThemeContextProvider>
+        <Navbar />
+        <Booklist />
+      </ThemeContextProvider>
     </>
   )
 }
