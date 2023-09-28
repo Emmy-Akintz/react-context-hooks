a26fad341308996d523dffbef804c99e80637d29
Ayomide Akintan <emzyakints2005@gmail.com>
2023-09-28 10:26:17 +0100

    Update

--- booklist/src/App.jsx
+++ booklist/src/App.jsx
@@ -1,6 +1,7 @@
 import NewBookForm from './components/BookForm'
 import BookList from './components/BookList'
 import Navbar from './components/Navbar'
+// import Reducers from './components/Reducers'
 import BookContextProvider from './contexts/BookContext'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <BookList />
         <NewBookForm />
       </BookContextProvider>
+      {/* <Reducers /> */}
     </>
   )
 }
